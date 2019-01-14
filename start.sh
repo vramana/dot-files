8e0cb44986d5e27df0dae9ccf61f07036f928f4e
Ramana Venkata <vramana@users.noreply.github.com>
2019-01-14 10:56:42 +0530

    Add initial script

---
+++ start.sh
@@ -0,0 +1,15 @@
+set -e
+
+sudo apt update
+
+sudo apt install python3-distutils
+
+sudo apt install vim
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+python3 get-pip.py --user
+
+
+
+
